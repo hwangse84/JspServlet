128b433a25d537211b4b92ff39d1e60671d250ec
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2023-11-16 18:01:13 +0900

    usertbl

---
+++ 03.Implicit/src/main/java/controller/UserTblController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import usertbl.UserTblDAO;
+import usertbl.UserTblDTO;
+//name,urlpatterns...
+
+//*.user,*.do....  :/를 빼야함(주의점)
+@WebServlet(name = "usertbl", urlPatterns = { "/list", "/detail","/update" }) // 여러가지 목록
+public class UserTblController extends HttpServlet {
+
+	// url요청이 list이면 전체목록을 list.jsp 가 보여주게함
+	// url요청이 detail이면 회원 한명의 정보를 detail.jsp가 보여주게함
+	// 요청에 따라 콘솔창에 list,detail이 찍히게 처리해보기
+	RequestDispatcher rd;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		
+		UserTblDAO dao = new UserTblDAO();
+		
+		if(req.getServletPath().equals("/list")) {
+			ArrayList<UserTblDTO> list = dao.selectList();
+			req.setAttribute("list", list);
+			rd = req.getRequestDispatcher("usertbl/list.jsp");
+			
+			
+		}else if(req.getServletPath().equals("/detail")) {
+			String name = req.getParameter("name");
+			UserTblDTO dto = dao.selectOne(name);
+			req.setAttribute("dto", dto);
+			rd = req.getRequestDispatcher("usertbl/detail.jsp");
+			
+			
+		}else if(req.getServletPath().equals("/update")){
+			UserTblDTO dto = new UserTblDTO();
+		
+			/* System.out.println(req.getParameter("username"));--먼져 확인해보기 */ 
+			dto.setUsername(req.getParameter("username"));
+			dto.setBirthyear(Integer.parseInt(req.getParameter("birthyear")));
+			dto.setMobile(req.getParameter("mobile"));
+			dto.setAddress(req.getParameter("address"));
+			//dao에 update 메소드 만들어보기.
+	
+		
+		rd.forward(req, resp);
+	}
+	}
+}
---
+++ 03.Implicit/src/main/java/usertbl/UserTblDAO.java
@@ -0,0 +1,109 @@
+package usertbl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UserTblDAO {
+
+	private Connection conn;
+	private PreparedStatement ps;
+	private ResultSet rs;
+
+	public boolean isConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "hanul";
+		String password = "0000";
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println(conn.isClosed());
+
+			if (!conn.isClosed())
+				return true;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+	
+	
+	
+//전체 목록조회!!!!
+	public ArrayList<UserTblDTO> selectList(){
+
+	ArrayList<UserTblDTO> list = new ArrayList<>();
+
+		if (!isConnection()) return list;
+
+		try {
+
+			ps = conn.prepareStatement("SELECT * FROM USERTBL");
+			rs = ps.executeQuery();
+			while (rs.next()) {
+
+				UserTblDTO dto = new UserTblDTO();
+				dto.setUsername(rs.getString("USERNAME"));
+				dto.setAddress(rs.getString("ADDRESS"));
+				dto.setBirthyear(rs.getInt("BIRTHYEAR"));
+				dto.setMobile(rs.getString("MOBILE"));
+				list.add(dto);
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+	
+	//데이터1건 조회!!
+	public UserTblDTO selectOne(String name) {
+		UserTblDTO dto= new UserTblDTO();
+		if (!isConnection()) return dto;
+
+		try {
+
+			ps = conn.prepareStatement("SELECT * FROM USERTBL WHERE USERNAME=?");
+			ps.setString(1, name);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+
+			
+				dto.setUsername(rs.getString("USERNAME"));
+				dto.setAddress(rs.getString("ADDRESS"));
+				dto.setBirthyear(rs.getInt("BIRTHYEAR"));
+				dto.setMobile(rs.getString("MOBILE"));
+				
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	return dto;	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
